dd51fad07e55fe36030999dd9421007abaf127ca
Nicolas Rio <nicolas.rio42@gmail.com>
2024-03-06 13:39:17 +0100

    Playing around with :has css selector

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-export async function load({ locals }) {
-	const user = locals.user;
-
-	return {
-		userId: user?.id ?? null,
-		userName: user?.username ?? null
-	};
-}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
 	import './pico-custom.css';
 	import 'uno.css';
-
-	export let data;
 </script>
 
 <nav class="container-fluid border-b-2 border-b-solid border-b-[var(--table-border-color)]">
@@ -14,18 +12,6 @@
 	</ul>
 
 	<ul>
-		{#if data.userId}
-			<li>
-				<a href="/profil" class="flex gap-2 items-center">
-					<i class="i-carbon-user-avatar block w-5 h-5" />
-
-					{data.userName}</a
-				>
-			</li>
-		{:else}
-			<li><a href="/login">Login</a></li>
-		{/if}
-
 		<li>
 			<ThemeSwitch />
 		</li>
