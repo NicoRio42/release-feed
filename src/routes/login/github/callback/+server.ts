0a5a9a598ce46b184c734d233420c49c79c05f89
Nicolas Rio <nicolas.rio42@gmail.com>
2024-04-27 19:41:16 +0200

    Printing response

--- src/routes/login/github/callback/+server.ts
+++ src/routes/login/github/callback/+server.ts
@@ -31,7 +31,7 @@ export const GET = async ({ url, cookies }) => {
 		console.log(
 			githubUserResponse.status,
 			githubUserResponse.statusText,
-			JSON.stringify(Object.fromEntries(githubUserResponse.headers))
+			await githubUserResponse.text()
 		);
 
 		const githubUser: GitHubUser = await githubUserResponse.json();
